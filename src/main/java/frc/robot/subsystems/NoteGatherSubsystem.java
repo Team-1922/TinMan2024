62a536e9954520484a0334a6460d66a0228e5d6b
Julian-Z <jzanetti26@sau66.org>
2024-01-16 20:17:54 -0500

    New branch
    Just set up some barebones stuff since there wasn't much to do at the moment.

---
+++ src/main/java/frc/robot/subsystems/NoteGatherSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+public class NoteGatherSubsystem extends SubsystemBase {
+
+    public NoteGatherSubsystem() {
+
+    }
+
+    public void Activate() {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+    
+}
