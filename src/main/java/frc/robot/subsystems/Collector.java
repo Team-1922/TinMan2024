a75baf09952b1adf9362363e6df11b01737445a5
Julian-Z <jzanetti26@sau66.org>
2024-01-27 13:25:07 -0500

    added second motor to collector

--- src/main/java/frc/robot/subsystems/Collector.java
+++ src/main/java/frc/robot/subsystems/Collector.java
@@ -7,6 +7,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Collector extends SubsystemBase {
     private static TalonFX m_CollectorTalon = new TalonFX(Constants.MotorConstants.kCollectorMotorID); 
+    private static TalonFX m_CollectorTalon2 = new TalonFX(Constants.MotorConstants.kCollectorSecondMotorID);
 
     public Collector() {
 
@@ -14,17 +15,23 @@ public class Collector extends SubsystemBase {
 
     public void ActivateMotor(double volts) {
         m_CollectorTalon.setVoltage(volts);   
+        m_CollectorTalon2.setVoltage(volts);
             m_CollectorTalon.setInverted(true);
+            m_CollectorTalon2.setInverted(true);
     }
 
     public void StopMotor() {
         m_CollectorTalon.setVoltage(0);
+        m_CollectorTalon2.setVoltage(0);
         m_CollectorTalon.disable();
+        m_CollectorTalon2.disable();
     }
 
 public void ReverseMotor(double volts) {
         m_CollectorTalon.setVoltage(volts);   
+        m_CollectorTalon2.setVoltage(volts);
         m_CollectorTalon.setInverted(false);
+        m_CollectorTalon2.setInverted(false);
     }
 
     @Override
