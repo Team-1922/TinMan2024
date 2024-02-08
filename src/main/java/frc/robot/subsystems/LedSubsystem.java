d5d67d9ef7ce34b7ef797d8e60f34015f82747a8
TeddyE-E <122415127+TeddyE-E@users.noreply.github.com>
2024-02-07 19:33:12 -0500

    added TOF

--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.led.Animation;
 import com.ctre.phoenix.led.CANdle;
 import com.ctre.phoenix.led.RainbowAnimation;
+import com.playingwithfusion.TimeOfFlight;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -15,11 +16,13 @@ import frc.robot.Constants;
 public class LedSubsystem extends SubsystemBase {
 
   CANdle m_CaNdle = new CANdle(Constants.LedConstants.CandleID); 
+  TimeOfFlight m_TOF = new TimeOfFlight(Constants.LedConstants.TOFid);
   RainbowAnimation m_rainbow = new RainbowAnimation(0.1, 0.1, Constants.LedConstants.TotalLedCount);
   /** Creates a new LedSubsystem. */
   public LedSubsystem() {
-
+  m_TOF.setRangeOfInterest(0, 0, 0, 0); //TODO update this 
   }
+  
 
   public void SetColor(int Red, int Green, int Blue, int White, int Start,int Count){// sets the leds to a specified color 
      
@@ -38,8 +41,14 @@ public class LedSubsystem extends SubsystemBase {
   }
 
 
+  public boolean TOFcheckTarget(){
+   return m_TOF.getRange() < Constants.LedConstants.TOFmaxRange && m_TOF.getRange() > Constants.LedConstants.TOFminRange;
+  }
 
-
-  
+  @Override
+  public void periodic() {
+  if (TOFcheckTarget()){SetColor(0, 255, 0, 0, 0, 8);}
+  else{SetColor(255, 0, 0, 0, 0, 8);}
+  }
 
 }
