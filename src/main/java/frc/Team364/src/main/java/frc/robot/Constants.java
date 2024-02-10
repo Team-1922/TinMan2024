9c6b552590574ca491c6f37737492e3d428c5ae1
TeddyE-E <122415127+TeddyE-E@users.noreply.github.com>
2024-02-10 14:14:18 -0500

    updated dimentions

--- src/main/java/frc/Team364/src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/Team364/src/main/java/frc/robot/Constants.java
@@ -26,8 +26,8 @@ public final class Constants {
             COTSFalconSwerveConstants.SDSMK4i(COTSFalconSwerveConstants.driveGearRatios.SDSMK4i_L3);
 
         /* Drivetrain Constants */  //13.625,17.625
-        public static final double trackWidth = Units.inchesToMeters(13.625); //TODO: This must be tuned to specific robot
-        public static final double wheelBase = Units.inchesToMeters(17.625); //TODO: This must be tuned to specific robot
+        public static final double trackWidth = Units.inchesToMeters(17.625); //TODO: This must be tuned to specific robot
+        public static final double wheelBase = Units.inchesToMeters(13.625); //TODO: This must be tuned to specific robot
         public static final double wheelCircumference = chosenModule.wheelCircumference;
 
         /* Swerve Kinematics 
