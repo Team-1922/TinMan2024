5dc009a9cf62b467682a1c45a40b9c98b134979a
TeddyE-E <122415127+TeddyE-E@users.noreply.github.com>
2024-02-10 16:06:57 -0500

    some progress

--- src/main/java/frc/robot/commands/CollectNote.java
+++ src/main/java/frc/robot/commands/CollectNote.java
@@ -5,28 +5,52 @@
 package frc.robot.commands;
 import frc.robot.subsystems.Collector;
 import frc.robot.Constants;
-
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class CollectNote extends Command {
   /** Creates a new CollectNote. */
   Collector m_Collector;
-
+  Timer m_Timer = new Timer();
+  boolean hadNote;
   public CollectNote(Collector Collector) {
     m_Collector = Collector;
     
     addRequirements(Collector);
+  
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_Collector.ActivateMotor(Constants.MotorConstants.kRollerVoltage);
+    if(!m_Collector.TOFcheckTarget()){
+      m_Collector.ReverseMotor(Constants.MotorConstants.kRollerVoltage);
+      hadNote= false;
+    }else{
+      hadNote= true;
+    }
+    m_Timer.reset();
+    m_Timer.start();
+  
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if (!hadNote) {
+      if (m_Collector.TOFcheckTarget()|| m_Timer.hasElapsed(.5)) {
+        m_Collector.StopMotor();
+      
+      }
+    }else{
+      if (m_Collector.TOFcheckTarget()) {
+         m_Collector.ActivateMotor(Constants.MotorConstants.kRollerVoltage);
+        
+        }
+      }
+     
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -37,6 +61,9 @@ public class CollectNote extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    if(m_Collector.TOFcheckTarget()){
+      
+    }
     return false;
   }
 }
