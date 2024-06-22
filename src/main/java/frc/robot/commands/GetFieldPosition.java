692789a88c4fd7fce9db9dc1a12c28553f2dc393
Julian <122415130+jzanetti26@users.noreply.github.com>
2024-06-22 12:10:24 -0400

    Issues with the robot
    robot pose will more than likely be implemented

--- src/main/java/frc/robot/commands/GetFieldPosition.java
+++ src/main/java/frc/robot/commands/GetFieldPosition.java
@@ -9,6 +9,8 @@ import java.util.function.Supplier;
 import java.util.ArrayList;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.PositionHandler;
 import frc.robot.subsystems.Vision;
@@ -26,8 +28,9 @@ public class GetFieldPosition extends Command {
   private double mRobotX;
   private double mRobotY;
   private ArrayList<Double> data;
+  private Timer m_Timer;
   Translation2d coolTranslation;
-
+  
   public GetFieldPosition(PositionHandler pos, Vision limelight, Swerve zoom) {
     m_LL = limelight;
     m_pos = pos;
@@ -42,34 +45,36 @@ public class GetFieldPosition extends Command {
   @Override
   public void initialize() {
     //Retrieve data, preferably with deferred execution
+    //get robot x and y
+    m_Timer.start();
     data = m_pos.returnData();
+    double offset = m_Swerve.getGyroYaw().getRadians(); //May get more innacurate with time. Robot pose may be used instead
+    double alpha = m_LL.cot(Math.toRadians(data.get(0))-offset);
+    double beta = m_LL.cot(Math.toRadians(data.get(1))-offset);
+    mRobotX = (mXRight*beta + mXLeft*alpha - mYRight + mYLeft)/(alpha + beta);
+    mRobotY = (mYRight*alpha + mYLeft*beta - mXRight + mXLeft)/(alpha + beta);
+    System.out.println(mRobotX); // 0 is set to the wall
+    System.out.println(mRobotY); // 0 is set to the center apriltag
   }
-
+  
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //get robot x and y
-    double alpha = m_LL.cot(Math.toRadians(data.get(0)));
-    double beta = m_LL.cot(Math.toRadians(data.get(1)));
-    mRobotX = (mXRight*beta + mXLeft*alpha - mYRight + mYLeft)/(alpha + beta);
-    mRobotY = (mYRight*alpha + mYLeft*beta - mXRight + mXLeft)/(alpha + beta);
+    coolTranslation = new Translation2d((-mRobotX + 1.5), (-mRobotY + 218.42)); //Weird translation; looking into this post-event
+    System.out.println(coolTranslation);
+    m_Swerve.drive(coolTranslation.times(0.0254), -m_Swerve.getGyroYaw().getRadians(), false, true);
+    // Teddy suggested having the shooting motors rev up here
   }
-
+  
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    System.out.println(mRobotX); // 0 is set to the wall
-    System.out.println(mRobotY); // 0 is set to the center apriltag
-    //Robot is not getting values from the limelight
-    coolTranslation = new Translation2d((-mRobotX + 0.5), (-mRobotY)); //Weird translation; looking into this post-event
-    System.out.println(coolTranslation);
-    //m_Swerve.drive(coolTranslation, 0 /* May or may not actually be zero */, false, true);
-    // Teddy suggested having the shooting motors rev up here
+    m_Timer.reset();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return true;
+    return (m_Timer.hasElapsed(0.5));
   }
 }
