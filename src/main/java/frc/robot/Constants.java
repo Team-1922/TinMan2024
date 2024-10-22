d259815fb3c99ea81b4bfb984cb6d8491627f12d
TeddyE-E <tehrmanntraut25@gmail.com>
2024-10-22 19:12:57 -0400

    comp changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -82,8 +82,8 @@ public final class Constants {
 
     public static final int TofRangeOfIntrest = 8; // don't change this 
     public static final int Tofid = 0; 
-    public static final int TofminRange = 25; // mm
-    public static final int TofmaxRange = 400; // mm
+    public static final int TofminRange = 0; // mm
+    public static final int TofmaxRange = 500; // mm
 
   }
 
